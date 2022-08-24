07dcf5531d7f24edfad1d1d58fad8c9d90ebc853
Dalma Fernandez <dalmagiufernandez@gmail.com>
2022-08-24 20:26:30 -0300

    Primet commit

---
+++ js/primeraclase.js
@@ -0,0 +1,30 @@
+console.log("Hola mundo");
+// poner ; es una buena practica
+document.write("Hola mundo");
+console.log("Este es un segundo mensaje");
+// prompt("Escriba un mensaje");
+// // console.log(prompt("Escriba un mensaje"));
+// let nombre = prompt("Escribe un nombre");
+// // asignacion lo de la derecha se guarda en lo de la izquierda
+// console.log(nombre);
+// // ley de murphy del programador: si el usuario puede hacer algo mal, lo hará mal
+// // debemos hacer validacion de los formularios
+// const PI = 3.14;
+// const o let se pone solamente cuando defino la variable
+// tipos de datos: numericos, texto, objetos, booleanos, hay mas
+// numericos:: int y float
+// texto: string (cadena de caracteres)
+// objetos: 
+// booleano: true y false
+let suma = 0;
+let valorDeEntrada = Number(prompt("Indique un numero"));
+suma = suma + valorDeEntrada;
+valorDeEntrada = Number(prompt("Indique un numero"));
+suma = suma + valorDeEntrada;
+valorDeEntrada = Number(prompt("Indique un numero"));
+suma = suma + valorDeEntrada;
+valorDeEntrada = Number(prompt("Indique un numero"));
+suma = suma + valorDeEntrada;
+console.log("La suma es " + suma );
+
+
